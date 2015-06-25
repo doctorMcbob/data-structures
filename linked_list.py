7ea927a28322d725d281d9855c17fbab97b9f16c
wesleywooten <wootenwesley@gmail.com>
2015-06-24 19:53:11 -0700

    fixed display method

--- linked_list.py
+++ linked_list.py
@@ -37,7 +37,7 @@ class LinkedList(object):
             i+=1
         
     def display(self): #return tuple literal of list items
-        t, i = (), 1
+        t, i = (), 0
         while i < len(self.ll):
             t = t[:i]+(self.ll[i], )+t[i:]
             print t
