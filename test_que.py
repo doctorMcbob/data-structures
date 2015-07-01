7fd5d2c932d73ca6f9ff6161ce8ed6696c1bfcda
HeyIamJames <jameshemmaplardh@gmail.com>
2015-06-30 17:11:09 -0700

    fixed linting errors

--- test_que.py
+++ test_que.py
@@ -1,5 +1,6 @@
 # enqueue(value): adds value to the queue
-# dequeue(): removes the correct item from the queue and returns its value (should raise an error if the queue is empty)
+# dequeue(): removes the correct item from the queue and returns its value
+# (should raise an error if the queue is empty)
 # size(): return the size of the queue.  Should return 0 if the queue is empty.
 
 import pytest
@@ -26,4 +27,4 @@ def test_size():
     q = Queue()
     for n in que:
         q.enqueue(n)
-        assert q.size() == n   
+        assert q.size() == n
