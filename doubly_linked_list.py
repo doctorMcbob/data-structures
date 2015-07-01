fc1087c4acbd699213bac95d7b19f24510c0dc1a
HeyIamJames <jameshemmaplardh@gmail.com>
2015-07-01 16:07:06 -0700

    fixed linting errors in doubly_linked_list and tests

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -1,11 +1,14 @@
 class Node(object):
+    """Creates a node"""
     def __init__(self, value=None, nextNode=None, prevNode=None):
-        self.value=value
-        self.nextNode=nextNode
-        self.prevNode=prevNode
+        self.value = value
+        self.nextNode = nextNode
+        self.prevNode = prevNode
+
 
 class DoubleLinkedList(object):
-    def __init__(self, iterable=None): 
+
+    def __init__(self, iterable=None):
         """
         Constructor:
           Accepts optional iterable
@@ -16,7 +19,7 @@ class DoubleLinkedList(object):
         self.last = None
         if iterable:
             self.first = Node(iterable[0], None)
-            node=self.first
+            node = self.first
             self._size += 1
             for val in iterable[1:]:
                 node.nextNode = Node(val, None)
@@ -25,7 +28,7 @@ class DoubleLinkedList(object):
                 self.last = node
                 self._size += 1
 
-    def insert(self, val): 
+    def insert(self, val):
         """
         insert method:
           adds new value to the head of the list
@@ -37,44 +40,43 @@ class DoubleLinkedList(object):
         self.first = self.first.prevNode
         self._size += 1
 
-    def pop(self): 
+    def pop(self):
         """
         pop method:
            removes the value at the head of the list and returns it
         """
-        node=self.first
+        node = self.first
         self.first = self.first.nextNode
         if self.first is not None:
             self.first.prevNode = None
         self._size -= 1
         return node.value
-    
-    def shift(self): 
+
+    def shift(self):
         """
         pop method:
            removes the value at the tail of the list and returns it
         """
-        node=self.last
+        node = self.last
         self.last = self.last.prevNode
         if self.last is not None:
             self.last.nextNode = None
         self._size -= 1
         return node.value
-        
-    def size(self): 
+
+    def size(self):
         return self._size
 
-    def remove(self, val): #remove node without index
+    def remove(self, val):
         """
         remove method:
           removes given node from list
         """
         node = self.first
-        while node.value != val:         
+        while node.value != val:
             node = node.nextNode
             if node is None:
-                raise ValueError(repr(val)+" is not present in DoublyLinkedList")
+                raise ValueError(repr(val) + " is not present in DoublyLinkedList")
         node.nextNode.prefNode = node.prevNode
         node.prevNode.nextNode = node.nextNode
         self._size -= 1
-            
