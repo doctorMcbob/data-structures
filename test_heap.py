ddeb5ef5ba270d02044ae0739999e0a74eb98383
HeyIamJames <jameshemmaplardh@gmail.com>
2015-07-02 16:37:00 -0700

    del comment

--- test_heap.py
+++ test_heap.py
@@ -3,10 +3,8 @@ from heap import BinaryHeap
 
 def test_push():
     heap = BinaryHeap((1, 3, 8, 26, 4, 15, 20, 88, 23, 7, 11, 40))
-    print heap.heapList
     assert heap.heapList[0] == 1
     assert heap.heapList[3] == 7
-    # assert heap.heapList[3] == 6
 
 
 def test_pop():
