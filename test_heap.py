ceedeb352887dd30d1b206cea656ffdfd51b2b00
wesleywooten <wootenwesley@gmail.com>
2015-07-20 14:42:51 -0700

    fixed up heap

--- test_heap.py
+++ test_heap.py
@@ -12,5 +12,5 @@ def test_pop():
     heap = BinaryHeap((1, 3, 8, 26, 4, 15, 20, 88, 23, 7, 11, 40))
     heap.pop()
     assert heap.heapList[1] == 3
-    assert heap.heapList[-2] == 40
+    assert heap.heapList[-2] == 7
     heap.pop()
